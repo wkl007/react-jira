589cdc10fd9fc3a403667bf1c786671b937fc987
wkl007 <499657357@qq.com>
2021-07-24 18:26:21 +0800

    feat: 实现 useArray hook

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,3 +24,19 @@ export const useDebounce = <T>(value: T, delay?: number): T => {
 
   return debounceValue;
 };
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
